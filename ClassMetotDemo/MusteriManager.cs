5e8b98661470ad61f09a7b01269d8d872fd983ba
Elif <elifintizam@gmail.com>
2021-01-20 17:54:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri uye)
+        {
+            Console.WriteLine(uye.Id +" "+ uye.Ad +" "+ uye.Soyad+" "+ uye.HesapNo +" "+ uye.HesaptakiPara);
+            Console.WriteLine("Müşteri Eklendi. \n");
+        }
+        public void Sil(Musteri uye)
+        {
+            Console.WriteLine(uye.HesapNo + " hesap numaralı müşteri silindi. \n");
+        }
+        public void Listele(Musteri uye)
+        {
+            Console.WriteLine("Id:" + uye.Id);
+            Console.WriteLine("Ad:" + uye.Ad);
+            Console.WriteLine("Soyad:" + uye.Soyad);
+            Console.WriteLine("Hesap No:" + uye.HesapNo);
+            Console.WriteLine("Hesaptaki Para:" + uye.HesaptakiPara);
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri uye1 = new Musteri();
+            uye1.Id = 235697;
+            uye1.Ad = "Elif";
+            uye1.Soyad = "Yılmaz";
+            uye1.HesapNo = 11111;
+            uye1.HesaptakiPara = 96.41;
+
+            Musteri uye2 = new Musteri(); 
+            uye2.Id = 457896;
+            uye2.Ad = "Ali";
+            uye2.Soyad = "Yıldırım";
+            uye2.HesapNo = 22222;
+            uye2.HesaptakiPara = 59;
+
+            Musteri uye3 = new Musteri();
+            uye3.Id = 129697;
+            uye3.Ad = "Ayşe";
+            uye3.Soyad = "Çiçek";
+            uye3.HesapNo = 33333;
+            uye3.HesaptakiPara = 100.45;
+
+            Musteri[] musteriler = new Musteri[] { uye1, uye2, uye3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Ekle(musteri);
+            }
+
+            musteriManager.Sil(uye3);
+
+            for (int i=0; i<musteriler.Length; i++)
+            {
+                musteriManager.Listele(musteriler[i]);
+            }
+        }
+    }
+}
